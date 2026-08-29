a6365ce24c0cc63ca2ef53d81fd44116db5f7476
Mokhtar Hussein Abdulwahab Alsorori <tarasana4const@gmail.com>
2026-08-29 10:08:03 +0300

    feat(academy): publish MVP under Lovable route

---
+++ src/routes/academy.verify.tsx
@@ -0,0 +1,7 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+import { App as TeacherAcademyApp } from "../../apps/teacher-academy/src/App";
+
+export const Route = createFileRoute("/academy/verify")({
+  component: TeacherAcademyApp,
+});
